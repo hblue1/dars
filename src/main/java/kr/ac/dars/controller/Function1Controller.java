607551c1da8875eaa8fc05fc3c056b26cbef6643
BlueLemonAd <len8586@gmail.com>
2022-06-27 20:05:30 +0900

    aa

--- src/main/java/kr/ac/dars/controller/Function1Controller.java
+++ src/main/java/kr/ac/dars/controller/Function1Controller.java
@@ -1,14 +1,14 @@
-package kr.ac.dars.controller;
+// package kr.ac.dars.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class Function1Controller {
-    @RequestMapping(value = "/Function1")
-    public String home(Model model)
-    {
-        return "Function1.html";
-    }
-}
+// @Controller
+// public class Function1Controller {
+//     @RequestMapping(value = "/Function1")
+//     public String home(Model model)
+//     {
+//         return "Function1.html";
+//     }
+// }
--- src/main/java/kr/ac/dars/controller/Function2Controller.java
+++ src/main/java/kr/ac/dars/controller/Function2Controller.java
@@ -1,14 +1,14 @@
-package kr.ac.dars.controller;
+// package kr.ac.dars.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class Function2Controller {
-    @RequestMapping(value = "/Function2")
-    public String home(Model model)
-    {
-        return "Function2.html";
-    }
-}
+// @Controller
+// public class Function2Controller {
+//     @RequestMapping(value = "/Function2")
+//     public String home(Model model)
+//     {
+//         return "Function2.html";
+//     }
+// }
