cea3111baa7775df2843c05537992bc8335c1550
BlueLemonAd <len8586@gmail.com>
2022-10-03 21:55:24 +0900

    test

--- src/main/java/kr/ac/dars/controller/FunctionSelectController.java
+++ src/main/java/kr/ac/dars/controller/FunctionSelectController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class FunctionController {
+public class FunctionSelectController {
     @RequestMapping(value = "/FunctionSelect")
     public String home(Model model)
     {
--- src/main/resources/static/js/FunctionSelect.js
+++ src/main/resources/static/js/FunctionSelect.js
@@ -3,21 +3,21 @@ $(document).ready(function(){
 })
 
 function Function1(){
-    window.location.href="/Function1";
+    window.location.href="/user/Function1";
 }
 
 function Function2(){
-    window.location.href="/Function2";
+    window.location.href="/user/Function2";
 }
 
 function Function3(){
-    window.location.href="/Function3";
+    window.location.href="/user/Function3";
 }
 
 function Function4(){
-    window.location.href="/Function4";
+    window.location.href="/user/Function4";
 }
 
 function Function5(){
-    window.location.href="/Function5";
+    window.location.href="/user/Function5";
 }
